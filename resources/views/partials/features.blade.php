bec53e6f4d030ec467c21836964b5ccd5d70cd8a
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-01-13 16:13:52 -0700

    Updated the welcome page content

--- resources/views/partials/features.blade.php
+++ resources/views/partials/features.blade.php
@@ -3,21 +3,21 @@
     <div>
       <h2 class="text-base font-semibold leading-6 tracking-wide text-blue-600 uppercase">Everything you need</h2>
       <p class="mt-2 text-3xl font-extrabold leading-9 text-gray-900">All-in-one platform</p>
-      <p class="mt-4 text-lg leading-7 text-gray-500">We provide a wide variety of services to small businesses. We understand everyone's needs are different. Here is a short summary of what we offer:</p>
+      <p class="mt-4 text-lg leading-7 text-gray-500">With a wide variety of services for small businesses, we understand everyone's needs are different. Here is a short summary of our services:</p>
     </div>
 
     <div class="mt-12 lg:mt-0 lg:col-span-2">
       <dl class="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-2 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
       <x-services service="Hosting">
-          We provide the cost effective hosting services according to your specific needs. Whether you have a simple WordPress site, or a large online business, we can provide the right hosting services for your customer base
+          Cost effective hosting services according to your specific needs. Whether you have a simple WordPress site, or a large online business, we'll provide the right hosting services for your application's needs
         </x-services>
 
         <x-services service="Web Design and SEO">
-          Need a brand new website, a redesign, or need to optimize your existing website for modern search engine results? We have you covered. We use the latest industry best practices to ensure your website is up to date in all the latest standards. We design every website to be mobile responsive, craft the content to maximize engagement, and dial in the site's performance rankings to get you listed in Google, Bing, Yahoo, and every other relevant search engine
+          Need a brand new website, a redesign, or need to optimize your existing website for modern search engine results? We use industry best practices to ensure your website is up to date in all the latest standards and design every website to be mobile responsive, craft the content to maximize engagement, and dial in the site's performance to get you listed in Google, Bing, Yahoo, and every other relevant search engine
         </x-services>
 
         <x-services service="Graphic Design">
-          We work with the very best to provide the very best in graphic design services. With over a decade of experience and countless happy customers, we can design your logos, fliers, posters, and all the in betweens
+          We partner with the very best to provide the very best in graphic design services. With over a decade of experience and countless happy customers, we can design your logos, fliers, posters, and all the in betweens
         </x-services>
 
         <x-services service="Social Media Marketing">
