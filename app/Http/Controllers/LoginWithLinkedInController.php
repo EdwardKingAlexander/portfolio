2b1cf2ea002bc1959d94850a111115e84b52c8c5
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-01-16 01:24:24 -0700

    linkedIn test 2

--- app/Http/Controllers/LoginWithLinkedInController.php
+++ app/Http/Controllers/LoginWithLinkedInController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class LoginWithLinkedInController extends Controller
