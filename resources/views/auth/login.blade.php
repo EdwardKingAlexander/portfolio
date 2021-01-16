c89dbd6b26f3feb69aee0906128f09c58a6b0159
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-01-16 00:54:39 -0700

    test for linkedin login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -151,13 +151,20 @@
           </div>
   
           <div class="mt-6">
-            <form action="#" method="POST" class="space-y-6">
+
+            @if (session('status'))
+            <div class="mb-4 text-sm font-medium text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+            <form action="{{ route('login') }}" method="POST" class="space-y-6">
+              @csrf
               <div>
                 <label for="email" class="block text-sm font-medium text-gray-700">
                   Email address
                 </label>
                 <div class="mt-1">
-                  <input id="email" name="email" type="email" autocomplete="email" required class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <input id="email" name="email" type="email" autocomplete="email" required class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                 </div>
               </div>
   
@@ -166,13 +173,13 @@
                   Password
                 </label>
                 <div class="mt-1">
-                  <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                 </div>
               </div>
   
               <div class="flex items-center justify-between">
                 <div class="flex items-center">
-                  <input id="remember_me" name="remember_me" type="checkbox" class="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500">
+                  <input id="remember_me" name="remember_me" type="checkbox" class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500">
                   <label for="remember_me" class="block ml-2 text-sm text-gray-900">
                     Remember me
                   </label>
@@ -186,7 +193,7 @@
               </div>
   
               <div>
-                <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-400 border border-transparent rounded-md shadow-sm hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-400 border border-transparent rounded-md shadow-sm hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                   Sign in
                 </button>
               </div>
