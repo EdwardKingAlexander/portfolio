66feabf46b707d9b2454d6f6a3f968e24b76e309
EdwardKingAlexander <Edward@RockyMountainWeb.Design>
2021-01-31 14:34:17 -0700

    updated error handling on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -157,6 +157,15 @@
                 {{ session('status') }}
             </div>
         @endif
+        @if ($errors->any())
+        <div class="text-red-500 text-semibold">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
             <form action="{{ route('login') }}" method="POST" class="space-y-6">
               @csrf
               <div>
@@ -164,7 +173,7 @@
                   Email address
                 </label>
                 <div class="mt-1">
-                  <input id="email" name="email" type="email" autocomplete="email" required class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                  <input id="email" name="email" type="email" autocomplete="email" value="{{ old('email') }}" required class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                 </div>
               </div>
   
--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,7 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\WhyUsController;
 use App\Http\Middleware\Honeypot;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 // Auth Routes
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
